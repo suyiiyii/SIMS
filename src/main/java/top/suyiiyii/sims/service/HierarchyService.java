9b95c1b70ad44bc6b426fb04df066d582303d540
suyiiyii <suyiiyii@gmail.com>
2024-09-08 21:33:20 +0800

    stash

---
+++ src/main/java/top/suyiiyii/sims/service/HierarchyService.java
@@ -0,0 +1,10 @@
+package top.suyiiyii.sims.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.stereotype.Service;
+import top.suyiiyii.sims.entity.HierarchyRelation;
+
+@Service
+public interface HierarchyService extends IService<HierarchyRelation> {
+
+}
---
+++ src/main/java/top/suyiiyii/sims/service/impl/HierarchyServiceImpl.java
@@ -0,0 +1,11 @@
+package top.suyiiyii.sims.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+import top.suyiiyii.sims.service.HierarchyService;
+
+@Service
+public class HierarchyServiceImpl implements ServiceImpl<Hierarchy> implements HierarchyService {
+
+}
